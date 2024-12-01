cdad87a55415310fd068eaea66eb9319a5bd7591
Сергей Петровых <sergio-p@yandex.ru>
2024-12-01 09:53:39 +0500

    11-5

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -3,27 +3,23 @@ import { useSelector } from 'react-redux';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
 import { useParams } from 'react-router-dom';
+import { RootState } from '../../services/rootReducer';
 import { getIngredientsSelector } from '../../services/slices/ingredientsSlice';
 
 export const IngredientDetails: FC = () => {
-  const { id } = useParams<{ id: string }>(); // Получаем id ингредиента из параметров маршрута
+  const { id } = useParams<{ id: string }>();
 
-  // Собираем все ингредиенты из состояния Redux
-  // const allIngredients = useSelector((state: RootState) => [
-  //   ...state.ingredients.buns,
-  //   ...state.ingredients.mains,
-  //   ...state.ingredients.sauces
-  // ]);
-
-  // Используем мемоизированный селектор
   const allIngredients = useSelector(getIngredientsSelector);
+  const loading = useSelector((state: RootState) => state.ingredients.loading);
+  const error = useSelector((state: RootState) => state.ingredients.error);
 
-  // Ищем ингредиент по id
-  const ingredientData = [...allIngredients].find(
+  if (loading) return <Preloader />;
+  if (error) return <p>Error: {error}</p>;
+
+  const ingredientData = allIngredients.find(
     (ingredient) => ingredient._id === id
   );
 
-  // Если ингредиент не найден, показываем лоадер
   if (!ingredientData) {
     return <Preloader />;
   }
